fee86be6f9a4084ca4464416ceca7da747908849
Lefin Thibault <thibault.lefin@insa-lyon.fr>
2026-03-23 23:05:14 +0100

    Initialisation refaite au propre du backend, seulement un exemple

---
+++ backend/src/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const { Pool } = pg;
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+
+    super({ adapter });
+  }
+  
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
